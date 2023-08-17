93b69141fcb27878f0e0bbbafbb075eb27fd2be2
utmalik <utmalik@amazon.com>
2023-08-17 09:19:54 +0530

    Lecture 8

---
+++ Javascript/Lecture8/sync.js
@@ -0,0 +1,101 @@
+/****
+ * Synchronous code-> the code that excutes line by line
+ * **/
+
+
+// console.log("Before");
+
+// function fn(){
+//     console.log("I am Fn");
+// }
+
+// fn();
+
+// console.log("after");
+
+/***
+ * Asynchronous code -> piece of code is executed at current point of time
+ *  and other piece of code is executed on later part
+ * 
+ * 
+ * */
+
+
+// console.log("Before");
+
+// function fn(){
+//     console.log("I am Fn");
+// }
+
+// setTimeout(fn, 2000);
+
+// console.log("after");
+
+
+/***
+ *  Environment : Browser
+ *  Web APIs :
+ *     console -> is not a part of JS
+ *     window -> is not a part of JS
+ *     document -> is not a part of JS
+ *     fetch -> is not a part of JS
+ *     I/O -> is not a part of JS
+ *     eventListeners -> is not a part of JS
+ * 
+ * Environment : Nodejs
+ *     fs: nodejs
+ *     http : nodejs
+ * 
+ * 
+ * 
+ */
+
+
+
+// let a = true;
+// console.log("Before");
+
+// setTimeout(() => {
+//     a = false;
+//     console.log("I broke the while loop");
+// }, 1000);
+// console.log("After");
+
+
+// while (a) {
+//     // console.log("inside while loop");
+// }
+
+
+// console.log("Before");
+
+// function fn(){
+//     console.log("I am Fn");
+// }
+
+// setTimeout(fn, 2000);
+
+// console.log("after");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
