5aadd098e1305fd2b198f3688e2401ee5b7ecf23
utmalik <utmalik@amazon.com>
2023-09-13 09:16:17 +0530

    autocomple finished

---
+++ Frontend_MC/Lecture7/autoComplete/fetchData.js
@@ -0,0 +1,47 @@
+let currentFetchController = null;
+
+async function getCountries(keyword){
+    try{
+
+        //some previous request is in flight
+        if(currentFetchController!==null){
+
+            //cancel the previous 
+            currentFetchController.abort();
+        }
+    
+    
+    let abbortController = new AbortController();
+
+    //assinging a new controller to currentFetchController
+    currentFetchController = abbortController;
+
+    const rawResponse = await fetch(`https://restcountries.com/v3.1/name/${keyword}`,{
+        signal:abbortController.signal
+    
+    });
+    console.log(rawResponse);
+
+    // i have recieved the response
+    currentFetchController=null;
+    
+    const response = await rawResponse.json();
+
+    if(rawResponse.status===200){
+        return response;
+    }
+
+    if(rawResponse.status==404){
+        console.log("Page not found");
+        return [];
+    }
+    return [];
+
+    }
+    catch(err){
+        console.log("Fetch Error:",err);
+    }
+}
+
+
+export default getCountries;
---
+++ Frontend_MC/Lecture7/autoComplete/readme.md
@@ -0,0 +1,6 @@
+## Optimisations
+
+- building list using fragments
+- implemented and used debouncing
+- to avoid older suggestion -> abort controller
+- caching your API request
